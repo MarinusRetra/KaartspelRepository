46d280d0415876f62c004037d62fe6e48fa55c50
Marinus Retra <marinus@flowonline.nl>
2024-09-09 16:50:54 +0200

    Init/Todo

---
+++ Kaartspel/Kaartspel/Program.cs
@@ -0,0 +1,29 @@
+﻿/*TO DO
+ * 1 Spelbord
+ * 2 Speler deck
+ * 3 AI deck
+ * 4 Kaarten vallen elkaar aan
+ * 5 Speler kan kaarten kiezen
+ * 6 Save functionaliteit
+ * 7 Speler en AI vallen elkaar direct aan
+ * 8 Kaarten class
+ * 9 kaarten kunnen aangepast worden met boosters
+ * 10 deck wordt groter narmate de speler verder raakt in het spel
+ * 11 speler kan winnen
+ * 12 speler kan doodgaan
+ * 13 Kaarten hebben speciale effecten
+ * 14 Kaarten wordenvan het spel gehaald als ze dood zijn
+ * 15 speler en AI krijgen een hand kaarten
+ * */
+
+/*De game is een kleine inscryption clone waarbij de speler met kaart battles de AI moet verslaan.
+De speler krijgt regelmatig nieuwe kaarten van de AI als de speler deze verslaat. Je kan de AI verslaan door 
+zijn levenspunten op te maken als de speler verliest is het game over en moet de speler opnieuw beginnen
+en begint het speler deck weer bij 8 kaarten.*/
+internal class Program
+{
+    static void Main(string[] args)
+    { 
+        Console.WriteLine("Hello, World!");
+    }
+}
